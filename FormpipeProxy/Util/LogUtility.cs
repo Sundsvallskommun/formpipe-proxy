1b21fa4e12658b1aa2ffd907bf4de56332d62257
cheezi0747 <linus.sjolinder@b3.se>
2025-08-11 13:23:18 +0200

    UF-16785: Santize logging

--- FormpipeProxy/Util/LogUtility.cs
+++ FormpipeProxy/Util/LogUtility.cs
@@ -1,16 +1,18 @@
+using System;
 using System.Text.RegularExpressions;
 
 namespace FormpipeProxy.Util
 {
     public class LogUtility
     {
+        private static readonly TimeSpan RegexTimeout = TimeSpan.FromSeconds(1);
+
         public static string SanitizeForLogging(string input) =>
             input is null ? null
                 : Regex.Replace(
                     Regex.Replace(
-                        Regex.Replace(input, "[\\r\\n]", " "), 
-                        "[^\\x20-\\x7E]", ""), 
-                    "[%\\\\]", "");
-
+                        Regex.Replace(input, @"[\r\n]", " ", RegexOptions.None, RegexTimeout),
+                        @"[^\x20-\x7E]", "", RegexOptions.None, RegexTimeout),
+                    @"[%\\]", "", RegexOptions.None, RegexTimeout);
     }
 }
